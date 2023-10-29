a20a923346b311a5ce356032b8f014d55d3e3631
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2023-10-29 14:43:49 +0530

    Create style file and add configuration

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("It works fine!!");
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("test")
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLG1CIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudC1wYWdlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwidGVzdFwiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
